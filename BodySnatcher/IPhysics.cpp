211cf7b089463c8e39e880db26fba81fc6514047
DanManGan <manganaro.daniel@gmail.com>
2014-04-12 10:02:39 +0800

    Finalized Physics Engine and added Doxygen Comments

--- BodySnatcher/IPhysics.cpp
+++ BodySnatcher/IPhysics.cpp
@@ -6,7 +6,7 @@ IPhysics::IPhysics(){
 	overlappingPairCache = new btDbvtBroadphase();
 	solver = new btSequentialImpulseConstraintSolver();
 	dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
-	dynamicsWorld->setGravity(btVector3(0,-10,0));		
+	dynamicsWorld->setGravity(btVector3(0,-9.81f,0));		
 }
 
 IPhysics::~IPhysics(){
@@ -20,18 +20,41 @@ void IPhysics::setGravity(float g){
 	dynamicsWorld->setGravity(btVector3(0, -g, 0));
 }
 
-void IPhysics::createGroundPlane(float xSize, float ySize, float zSize, float xPos, float yPos, float zPos){
+btCollisionShape* IPhysics::createGroundPlane(float xSize, float ySize, float zSize, float xPos, float yPos, float zPos){
 	btCollisionShape* groundShape = new btStaticPlaneShape(btVector3(xSize,ySize,zSize),1);
 	collisionShapes.push_back(groundShape);
 
 	btTransform groundTransform;
 	groundTransform.setIdentity();
 	groundTransform.setOrigin(btVector3(xPos, yPos, zPos));
+
+	return groundShape;
 }
 
-void IPhysics::createBoundingBox(float x, float y, float z){
-	btCollisionShape* boxShape = new btBoxShape(btVector3(btScalar(x),btScalar(y),btScalar(z)));
+btRigidBody* IPhysics::createBox(float sx, float sy, float sz, float xPos, float yPos, float zPos, float mass){
+	btCollisionShape* boxShape = new btBoxShape(btVector3(btScalar(sx),btScalar(sy),btScalar(sz)));
 	collisionShapes.push_back(boxShape);
+
+	btTransform startTransform;
+	startTransform.setIdentity();
+
+	btScalar tMass(mass);
+
+	//rigidBody is dynamic if and only if mass is non zero otherwise state
+	bool isDynamic = (tMass != 0.f);
+
+	btVector3 localInertia(0,0,0);
+	if(isDynamic)
+		boxShape->calculateLocalInertia(tMass,localInertia);
+
+	startTransform.setOrigin(btVector3(xPos,yPos,xPos));
+
+	//using motionstate is recommended, it provides interpolation capabilities, and only synchronizes 'active' objects
+	btDefaultMotionState* myMotionState = new btDefaultMotionState(startTransform);
+	btRigidBody::btRigidBodyConstructionInfo rbInfo(tMass, myMotionState, boxShape, localInertia);
+	btRigidBody* body = new btRigidBody(rbInfo);
+	dynamicsWorld->addRigidBody(body);
+	return body;
 }
 
 void IPhysics::deleteCollisionShapes(){
@@ -42,8 +65,7 @@ void IPhysics::deleteCollisionShapes(){
 	}
 }
 
-bool IPhysics::collision(btManifoldPoint& cp, const btCollisionObject* obj){
-
-	return false;
+void IPhysics::simulate(float timeStep){
+	dynamicsWorld->stepSimulation(btScalar(timeStep), 10);
 }
 
--- BodySnatcher/IPhysics.h
+++ BodySnatcher/IPhysics.h
@@ -22,22 +22,53 @@ private:
 	btAlignedObjectArray<btCollisionShape*> collisionShapes;
 public:
 	/*
-	* Constructor
+	* @brief Constructor
 	*/
 	IPhysics();
 	/*
-	* Deconstructor
+	* @brief Deconstructor
 	*/
 	~IPhysics();
-	void createBoundingBox(float, float, float);
-	void transformBoundingBox(btCollisionShape*);
-	void setGravity(float);
-	void createGroundPlane(float, float, float, float, float, float);
-	void addRigidBody();
+	/**
+	* @brief create a collision box 
+	* @param float sx - x scale of box 
+	* @param float sy - y scale of box
+	* @param float sz - z scale of box 
+	* @oaram float xPoz - x position of box
+	* @param float yPos - y position of box
+	* @oaram float zPos - z position of box
+	* @param float mass - mass of the box
+	* @return btRigigBody* - a pointer to the new box
+	*/
+	btRigidBody* createBox(float sx, float sy, float sz, float xPos, float yPos, float zPos, float mass);
+	/**
+	* @brief sets the gravity of the world
+	* @param float - gravity of the world
+	* @return void
+	*/
+	void setGravity(float g);
+	/**
+	* @brief creates the ground plane of the world
+	* @param float sx - x scale of ground plane 
+	* @param float sy - y scale of ground plane
+	* @param float sz - z scale of ground plane
+	* @oaram float xPoz - x position of ground plane
+	* @param float yPos - y position of ground plane
+	* @oaram float zPos - z position of ground plane
+	* @return btCollisionShape* - a pointer to the new ground plane
+	*/
+	btCollisionShape* createGroundPlane(float xSize, float ySize, float zSize, float xPos, float yPos, float zPos);
+	/**
+	* @brief deletes all collision shapes created
+	* @return void
+	*/
 	void deleteCollisionShapes();
-	bool collision(btManifoldPoint&, const btCollisionObject*);
-
-	
+	/**
+	* @brief simulares a single frame, goes in an Update function
+	* @param float timeStep - time between each step
+	* @return void
+	*/
+	void simulate(float timeStep);
 };
 
 #endif
