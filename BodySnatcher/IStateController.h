e159bf9c32f873e02fe66d92f9c1111a48a4198e
DanManGan <manganaro.daniel@gmail.com>
2014-04-13 20:04:50 +0800

    Interface for State Controller Added

---
+++ BodySnatcher/IStateController.h
@@ -0,0 +1,67 @@
+/**
+ * @class IStateController
+ * @brief Interface defines a StateController with its functions
+ *
+ * Defines the basic functions a StateController that interacts with the user should implement such as Init(), Update(), Draw()
+ *
+ * @author Daniel
+ * @version 01
+ * @date 13/04/2014 Daniel Manganaro - Basic Implementation
+ */
+
+#pragma once
+
+class IStateController {
+public:	
+	IStateController* mNextState;
+	/**
+		* @brief empty, virtual destructor that makes sure that the class will clear the memory
+		* virtual is important since the class should be cleared even if there is there is a list of pointers of it
+	*/
+	virtual ~IStateController() {};
+	IStateController(): mNextState(nullptr) {};
+	/**
+		* @brief abstract, virtual init-function
+	*/
+	virtual void Init() = 0;
+	/**
+		* @brief abstract, virtual Update function for the state 
+	*/
+	virtual void Update() = 0;
+	/**
+		* @brief abstract, virtual Draw function for the state
+	*/
+	virtual void Draw() = 0;
+	/**
+		* @brief abstract, virtual SpecialKey function for the state
+	*/
+	virtual void SpecialKey(int key, int x, int y) = 0;
+	/**
+		* @brief abstract, virtual SpecialKeyUp function for the state
+	*/
+	virtual void SpecialKeyUp(int key, int x, int y) = 0;
+	/**
+		* @brief abstract, virtual KeyboardUp function for the state
+	*/
+	virtual void KeyboardUp(unsigned char key, int x, int y) = 0;
+	/**
+		* @brief abstract, virtual Keyboard function for the state
+	*/
+	virtual void Keyboard(unsigned char key, int x, int y) = 0;
+	/**
+		* @brief abstract, virtual Mouse function for the state
+	*/
+	virtual void Mouse(int button, int state, int x, int y) = 0;
+	/**
+		* @brief abstract, virtual PassiveMotion function for the state
+	*/	
+	virtual void PassiveMotion(int x, int y) = 0;
+	/**
+		* @brief abstract, virtual MouseMotion function for the state
+	*/
+	virtual void MouseMotion(int x, int y) = 0;
+	/**
+		* @brief abstract, virtual Reshape function for the state
+	*/
+	virtual void Reshape(int w, int h) = 0;
+};
